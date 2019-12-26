146eb08287638e5e4a9cc042bdd340ac22e95abf
pakawat <pakawatkut@gmail.com>
2019-12-26 21:11:13 +0700

    font-end update home add partials folder , add head , header , footer ,nav

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const port = 7000
 const routes = require('./routes')
 
 app.set('view engine','ejs')
-app.use(express.static(path.join(__dirname,'public')))
-
+// app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static('./public'))
 
 app.get('/',routes.home)
 
